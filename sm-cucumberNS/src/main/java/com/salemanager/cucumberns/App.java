fa2049df7f31f28cb6c8b5cebd55707a6cec7b8b
naser <nasser4@yahoo.com>
2017-06-01 13:00:29 +0100

    added Cucumber tests

---
+++ sm-cucumberNS/src/main/java/com/salemanager/cucumberns/App.java
@@ -0,0 +1,13 @@
+package com.salemanager.cucumberns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
