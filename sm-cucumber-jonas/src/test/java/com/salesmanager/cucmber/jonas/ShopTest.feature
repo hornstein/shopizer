e2a066c6466beff5cc0815cc8d4a895d2498158f
Jonas Lindberg <jonaslindberg67@hotmail.com>
2017-06-04 16:46:27 +0200

    Update tests

--- sm-cucumber-jonas/src/test/java/com/salesmanager/cucmber/jonas/ShopTest.feature
+++ sm-cucumber-jonas/src/test/java/com/salesmanager/cucmber/jonas/ShopTest.feature
@@ -21,4 +21,35 @@ Then I should be on contact us page
 @Selenium
 Scenario: Test signin function
 Given I am on shopizer website
-And 
+And I click on signin
+And I fill in password and username
+And I click on login
+Then I should get an error message
+
+@Selenium
+Scenario: Test sortby function
+Given I am under businesslink in shopizer website
+And I click on sortby
+Then I should se Name
+
+@Selenium
+Scenario: Test add to cart function
+Given I am on shopizer website
+And I click on add to cart
+And I click on open cart
+Then I check that the book is added
+
+@Selenium
+Scenario: Test the checkout function
+Given I am on shopizer website
+And I add the book Node Web Development
+And I click on open cart and checkout
+Then I review my order
+
+@Selenium
+Scenario: Test remove function
+Given I am on shopizer website
+And I add a book
+And I click on open cart to see the added book
+And I click on remove
+Then I receive and error message
--- sm-cucumber-jonas/src/test/java/com/salesmanager/cucmber/jonas/ShopTest.java
+++ sm-cucumber-jonas/src/test/java/com/salesmanager/cucmber/jonas/ShopTest.java
@@ -88,7 +88,163 @@ public class ShopTest {
 		
 		driver.close();
 	}
-		
+		@And("^I click on signin$")
+		public void signIn(){
+			WebElement sign = driver.findElement(By.xpath(".//*[@id='customerAccount']/button"));
+			sign.click();
+		}
+		@And("^I fill in password and username$")
+		public void passAndUserName(){
+			WebElement usern = driver.findElement(By.id("signin_userName"));
+			usern.sendKeys("Jonas");
+			
+			WebElement passw = driver.findElement(By.id("signin_password"));
+			passw.sendKeys("12345");
+		}
+		@And("^I click on login$")
+		public void logIn() throws InterruptedException{
+			WebElement login = driver.findElement(By.xpath(".//*[@id='login-button']"));
+			login.click();
+			
+			Thread.sleep(2000);
+		}
+		@Then("^I should get an error message$")
+		public void errorM(){
+			WebElement error = driver.findElement(By.xpath(".//*[@id='loginError']"));
+			assertEquals("Login Failed. Username or Password is incorrect.", error.getText());
+			System.out.println(error.getText());
+			
+			driver.close();
+		}
+		@Given("^I am under businesslink in shopizer website$")
+		public void sortby(){
+			driver.get("http://jenkins2017.westeurope.cloudapp.azure.com:8080/shop/category/business.html/ref=c:6");
+		}
+		@And("^I click on sortby$")
+		public void sb(){
+			WebElement nm = driver.findElement(By.xpath("//*[@value='item-name']"));
+			nm.click();
+		}
+		@Then("^I should se Name$")
+		public void nm(){
+			WebElement nm = driver.findElement(By.xpath("//*[@value='item-name']"));
+			assertEquals("Name", nm.getText());
+			driver.close();
+		}
+		@And("^I click on add to cart$")
+		public void atc(){
+			WebElement add = driver.findElement(By.xpath( ".//*[@id='pageContainer']/div[2]/div[3]/div[2]/div/div[3]/div/div[1]/a/img"));
+			add.click();
+			
+			WebDriverWait w = new WebDriverWait(driver, 10);
+			w.until(ExpectedConditions.elementToBeClickable(By.xpath(".//*[@id='input-2']/div/button")));
+			
+			WebElement button = driver.findElement(By.xpath( ".//*[@id='input-2']/div/button"));
+			button.click();
+			System.out.println("Book Node Web Development has been added to shopping cart");
+		}
+		@And("^I click on open cart$")
+		public void oc(){
+			JavascriptExecutor jse = (JavascriptExecutor)driver;
+			jse.executeScript("scroll(0, -250);");
+			
+			WebDriverWait wait = new WebDriverWait(driver, 10);
+			wait.until(ExpectedConditions.elementToBeClickable(By.xpath(".//*[@id='open-cart']")));
+			
+			WebElement cart = driver.findElement(By.xpath(".//*[@id='open-cart']"));
+			cart.click();
+		}
+		@Then("^I check that the book is added$")
+		public void check(){
+			WebElement book = driver.findElement(By.xpath(".//*[@id='2']/td[2]"));
+			book.getText();
+			assertEquals("1 Node Web Development", book.getText());
+			
+			driver.close();
+		}
+		@And("^I add the book Node Web Development$")
+		public void addNwd(){
+			WebElement add = driver.findElement(By.xpath( ".//*[@id='pageContainer']/div[2]/div[3]/div[2]/div/div[3]/div/div[1]/a/img"));
+			add.click();
+			
+			WebDriverWait wait = new WebDriverWait(driver, 10);
+			wait.until(ExpectedConditions.elementToBeClickable(By.xpath( ".//*[@id='input-2']/div/button")));
+			
+			WebElement button = driver.findElement(By.xpath( ".//*[@id='input-2']/div/button"));
+			button.click();
+			System.out.println("Book Node Web Development has been added to shopping cart");
+		}
+		@And("^I click on open cart and checkout$")
+		public void checkO(){
+			JavascriptExecutor jse = (JavascriptExecutor)driver;
+			jse.executeScript("scroll(0, -250);");
+			
+			WebDriverWait wai = new WebDriverWait(driver, 10);
+			wai.until(ExpectedConditions.elementToBeClickable(By.xpath(".//*[@id='open-cart']")));
+			
+			WebElement cart = driver.findElement(By.xpath(".//*[@id='open-cart']"));
+			cart.click();
+			
+			WebElement co = driver.findElement(By.xpath(".//*[@id='miniCartDetails']/li/div/a"));
+			co.click();
+			
+			WebDriverWait wa = new WebDriverWait(driver, 10);
+			wa.until(ExpectedConditions.elementToBeClickable(By.className("cart-title")));
+		}
+		@Then("^I review my order$")
+		public void review(){
+			WebElement rev = driver.findElement(By.className("cart-title"));
+			rev.getText();
+			assertEquals("Review your order", rev.getText());
+			
+			driver.close();
+		}
+		@And("^I add a book$")
+		public void aAb(){
+			WebElement add = driver.findElement(By.xpath( ".//*[@id='pageContainer']/div[2]/div[3]/div[2]/div/div[3]/div/div[1]/a/img"));
+			add.click();
+			
+			WebDriverWait wait = new WebDriverWait(driver, 10);
+			wait.until(ExpectedConditions.elementToBeClickable(By.xpath( ".//*[@id='input-2']/div/button")));
+			
+			WebElement button = driver.findElement(By.xpath( ".//*[@id='input-2']/div/button"));
+			button.click();
+			System.out.println("Book Node Web Development has been added to shopping cart");
+		}
+		@And("^I click on open cart to see the added book$")
+		public void cart(){
+			JavascriptExecutor jse = (JavascriptExecutor)driver;
+			jse.executeScript("scroll(0, -250);");
+			
+			WebDriverWait wai = new WebDriverWait(driver, 10);
+			wai.until(ExpectedConditions.elementToBeClickable(By.xpath(".//*[@id='open-cart']")));
+			
+			WebElement cart = driver.findElement(By.xpath(".//*[@id='open-cart']"));
+			cart.click();
+		}
+		@And("^I click on remove$")
+		public void rm(){
+			WebElement co = driver.findElement(By.xpath(".//*[@id='miniCartDetails']/li/div/a"));
+			co.click();
+			
+			WebDriverWait w = new WebDriverWait(driver, 10);
+			w.until(ExpectedConditions.elementToBeClickable(By.xpath(".//*[@id='mainCartTable']/tbody/tr[1]/td[5]/button")));
+			
+			WebElement r = driver.findElement(By.xpath(".//*[@id='mainCartTable']/tbody/tr[1]/td[5]/button"));
+			r.click();
+			
+			WebDriverWait er = new WebDriverWait(driver, 10);
+			er.until(ExpectedConditions.elementToBeClickable(By.xpath(".//*[@id='pageContainer']/table/tbody/tr[1]/td[2]/h3")));
+		}
+		@Then("^I receive and error message$")
+		public void erM(){
+			WebElement error = driver.findElement(By.xpath(".//*[@id='pageContainer']/table/tbody/tr[1]/td[2]/h3"));
+			error.getText();
+			assertEquals("An error occurred in this request", error.getText());
+			System.out.println(error.getText());
+			
+			driver.close();
+		}
 }		
 	
 
