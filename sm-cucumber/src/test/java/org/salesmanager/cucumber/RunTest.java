439420a9323f3fe45e6074b05148e78e16fca7a8
Daniel Åkerblom <daniel_big_boy@hotmail.com>
2017-08-28 10:37:22 +0200

    fixat

--- sm-cucumber/src/test/java/org/salesmanager/cucumber/RunTest.java
+++ sm-cucumber/src/test/java/org/salesmanager/cucumber/RunTest.java
@@ -3,9 +3,10 @@ package org.salesmanager.cucumber;
 import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
 
 
-@RunWith(Cucumber.class) 
+@RunWith(CucumberWithSerenity.class) 
 @CucumberOptions( 
     features = "src/test/java/org/salesmanager/cucumber" 
     ,glue={"classpath:"} 
