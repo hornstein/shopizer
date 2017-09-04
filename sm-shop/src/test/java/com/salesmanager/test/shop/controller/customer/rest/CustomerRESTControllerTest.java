39c2e106ee40bda5deec41a06462d6e1b57aea6e
mikaelajenas <mikaela.jenas@hotmail.com>
2017-09-04 11:32:28 +0200

    fixed conflicts

--- sm-shop/src/test/java/com/salesmanager/test/shop/controller/customer/rest/CustomerRESTControllerTest.java
+++ sm-shop/src/test/java/com/salesmanager/test/shop/controller/customer/rest/CustomerRESTControllerTest.java
@@ -161,7 +161,7 @@ json="{\"id\" : 0,\"emailAddress\" : \"carl@csticonsulting.com\",\"billing\" : {
 		HttpEntity<String> entity = new HttpEntity<String>(json, getHeader());
 
 		//ResponseEntity response = restTemplate.postForEntity("http://localhost:8080/sm-shop/services/private/DEFAULT/customer", entity, PersistableCustomer.class);
-		ResponseEntity response = restTemplate.postForEntity("http://localhost:8080/services/private/DEFAULT/customer", entity, CustomerEntity.class);
+		ResponseEntity response = restTemplate.postForEntity("http://bluebottle.westeurope.cloudapp.azure.com:8080/services/private/DEFAULT/customer", entity, CustomerEntity.class);
 
 		Customer cust = (Customer) response.getBody();
 		System.out.println("New Customer ID : " + cust.getId());
